f019a56746d21f737f1ac9bd2afeb4f99ae794de
thickhoctin <thickhoctin@gmail.com>
2025-03-21 23:50:02 +0100

    Shared_wallet

---
+++ contracts/Allowance.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Allowance is Ownable{    
+    event AllowanceChanged(address indexed _forWho, address indexed _byWhom, uint _oldAmount, uint _newAmount);
+    mapping(address => uint) public allowance;
+    address initialOwner;
+    constructor() Ownable(msg.sender) {
+        initialOwner = msg.sender;
+    }
+
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
+    }
+
+    function setAllowance(address _who, uint _amount) public onlyOwner() {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount, "You are not allowed!");
+        _;
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal ownerOrAllowed(_amount) {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] -= _amount;
+    }
+
+    
+}
+
